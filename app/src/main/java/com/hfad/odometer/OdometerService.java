cf23419cd9405b17172021ac48e6fd246a8d1648
Rashedul Haydar <rh2712@columbia.edu>
2021-01-23 19:45:05 -0500

    Ch. 19: Initial commit of Odometer app.

---
+++ app/src/main/java/com/hfad/odometer/OdometerService.java
@@ -0,0 +1,13 @@
+package com.hfad.odometer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class OdometerService extends Service {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
