5457027355c676dd65388e0d0b71a6e6ec6b31f9
Rashedul Haydar <rh2712@columbia.edu>
2021-01-24 22:41:06 -0500

    Ch. 19: Request for location permission if not already given.
    If the user denies the permission, then show a notification saying that the
    permission is required.

--- app/src/main/java/com/hfad/odometer/MainActivity.java
+++ app/src/main/java/com/hfad/odometer/MainActivity.java
@@ -1,11 +1,14 @@
 package com.hfad.odometer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
@@ -13,8 +16,17 @@ import android.widget.TextView;
 
 import java.util.Locale;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
+
 public class MainActivity extends AppCompatActivity {
 
+    private final int PERMISSION_REQUEST_CODE = 698;
+    public static final int NOTIFICATION_ID = 5453;
+    public static final String CHANNEL_ID = "joke";
     private OdometerService odometer;
     private boolean bound = false;
 
@@ -43,8 +55,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Intent intent = new Intent(this, OdometerService.class);
-        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+        if (ContextCompat.checkSelfPermission(this, OdometerService.PERMISSION_STRING)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[] {OdometerService.PERMISSION_STRING},
+                    PERMISSION_REQUEST_CODE);
+        } else {
+            Intent intent = new Intent(this, OdometerService.class);
+            bindService(intent, connection, Context.BIND_AUTO_CREATE);
+        }
     }
 
     @Override
@@ -53,6 +72,47 @@ public class MainActivity extends AppCompatActivity {
         if (bound) {
             unbindService(connection);
             bound = false;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                          String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(this, OdometerService.class);
+                    bindService(intent, connection, Context.BIND_AUTO_CREATE);
+                } else {
+                    /* Issue a notification that the app can't run without location
+                     * permissions.
+                     */
+
+                    /* Create a notification channel before building the notification. */
+                    createNotificationChannel();
+
+                    /* Create a notification builder. */
+                    NotificationCompat.Builder builder =
+                            new NotificationCompat.Builder(this, CHANNEL_ID)
+                                    .setSmallIcon(android.R.drawable.ic_menu_compass)
+                                    .setContentTitle(getString(R.string.app_name))
+                                    .setContentText(getString(R.string.permission_denied))
+                                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                                    .setVibrate(new long[] {1000, 1000})
+                                    .setAutoCancel(true);
+
+                    /* Create an action. */
+                    Intent actionIntent = new Intent(this, MainActivity.class);
+                    PendingIntent pendingActionIntent = PendingIntent.getActivity(this, 0,
+                            actionIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                    builder.setContentIntent(pendingActionIntent);
+
+                    /* Issue the notification. */
+                    NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+                    notificationManager.notify(NOTIFICATION_ID, builder.build());
+                }
+            }
         }
     }
 
@@ -72,5 +132,22 @@ public class MainActivity extends AppCompatActivity {
                 handler.postDelayed(this, 1000);
             }
         });
+    }
+
+    /* From https://developer.android.com/training/notify-user/build-notification#java */
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence channelName = getString(R.string.app_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, channelName, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
